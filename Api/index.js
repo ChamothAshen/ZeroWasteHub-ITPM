ff8ba7c51a9cacbb9c7f08902997e8fee965b085
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2025-03-17 14:57:27 +0530

    connect mongo db

---
+++ Api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.connect(
+  process.env.MONGOUrl).then(() => {
+  console.log('connected to db');
+}).catch((err) => {
+  console.log(err);
+})
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
