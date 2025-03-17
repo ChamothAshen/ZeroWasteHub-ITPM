72f21d7ec025041bd66acc921d66d86302f40120
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2025-03-17 11:09:12 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className='text text-3xl'>app</div>
+    <BrowserRouter>
+       <Header />
+       <Routes>
+    
+       <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+       </Routes>
+         
+    </BrowserRouter>
+  
   )
 }
 //test
