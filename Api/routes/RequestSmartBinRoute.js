53352f385e7742498676a556358bba0a22af8a73
NethmiT <nethmi.thambawita@dmselectronics.com>
2025-04-28 22:59:36 +0530

    delete coments

--- Api/routes/RequestSmartBinRoute.js
+++ Api/routes/RequestSmartBinRoute.js
@@ -1,15 +1,4 @@
-// // routes/RequestSmartBinRoutes.js
-// import express from 'express';
-// import  {createSmartBinRequest}  from '../controllers/RequestSmartBinController.js';
 
-// const router = express.Router();
-
-// // Create a new smart bin request
-// router.post('/', createSmartBinRequest);
-
-// export default router;
-
-// routes/RequestSmartBinRoutes.js
 import express from 'express';
 import  {createSmartBinRequest, getAllSmartBinRequests,getSmartBinRequestById}  from '../controllers/RequestSmartBinController.js';
 
