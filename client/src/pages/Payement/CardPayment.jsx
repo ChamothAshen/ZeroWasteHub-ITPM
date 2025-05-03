b33db578dfe82b538378cede799fd1676aeb7e86
NethmiT <nethmi.thambawita@dmselectronics.com>
2025-05-03 22:02:48 +0530

    comentssss

--- client/src/pages/Payement/CardPayment.jsx
+++ client/src/pages/Payement/CardPayment.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-
+//
 const CardPayment = () => {
   const navigate = useNavigate();
   const { requestId } = useParams();
